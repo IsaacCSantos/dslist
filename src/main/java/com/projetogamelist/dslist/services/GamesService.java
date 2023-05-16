2d20bef45ae1df3c133560797b0d01d5c38a80f1
Isaac <isaac.correia@outlook.com.br>
2023-05-16 00:09:09 -0300

    Domain model

--- src/main/java/com/projetogamelist/dslist/services/GamesService.java
+++ src/main/java/com/projetogamelist/dslist/services/GamesService.java
@@ -17,7 +17,6 @@ public class GamesService {
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-		return dto;
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
